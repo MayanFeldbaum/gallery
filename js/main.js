76bc3bceceee511b4744eeba41434ff22987356d
MayanFeldbaum <mayan.lemberg@gmail.com>
2022-12-05 17:14:52 +0200

    rendering portfolio modal link

--- js/main.js
+++ js/main.js
@@ -1,15 +1,17 @@
 
 $(document).ready(init)
 
+
 function init() {
-    renderProjs()
+  renderProjs()
+  renderModal()
 }
 
 function renderProjs() {
     const projs = getProjs()
     const strHTMLs = projs.map(proj => {
         return `<div class="col-md-4 col-sm-6 portfolio-item">
-          <a class="portfolio-link" data-toggle="modal" href="#portfolioModal1">
+          <a class="portfolio-link" data-toggle="modal" href="#portfolioModal${proj.id}">
             <div class="portfolio-hover">
               <div class="portfolio-hover-content">
                 <i class="fa fa-plus fa-3x"></i>
@@ -22,6 +24,51 @@ function renderProjs() {
             <p class="text-muted">${proj.title}</p>
           </div>
         </div>`
-    })
+            })
     $('.projects-container').html(strHTMLs)
 }
+
+function renderModal() {
+  const projs = getProjs()
+  const strHTMLs = projs.map(proj => {
+      return `<div class="portfolio-modal modal fade" id="portfolioModal${proj.id}" tabindex="-1" role="dialog" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="close-modal" data-dismiss="modal">
+            <div class="lr">
+              <div class="rl"></div>
+            </div>
+          </div>
+          <div class="container">
+            <div class="row">
+              <div class="col-lg-8 mx-auto">
+                <div class="modal-body">
+                  <!-- Project Details Go Here -->
+                  <h2>${proj.name}</h2>
+                  <p class="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
+                  <img class="img-fluid d-block mx-auto" src="img/portfolio/01-full.jpg" alt="">
+                  <p>Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis
+                    dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate,
+                    maiores repudiandae, nostrum, reiciendis facere nemo!</p>
+                  <ul class="list-inline">
+                    <li>Date: January 2017</li>
+                    <li>Client: Threads</li>
+                    <li>Category: Illustration</li>
+                  </ul>
+                  <a href="https://mayanfeldbaum.github.io/${proj.id}/" class="btn btn-info" role="button" target="_blank">Check it out!</a>
+                  <button class="btn btn-primary btn-projectLink" data-dismiss="modal" type="button">
+                      <i class="fa fa-times"></i>
+                      Close Project</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>`
+  })
+  $('.modals').html(strHTMLs)
+}
+
+
+
